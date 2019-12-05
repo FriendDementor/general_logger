37a3710230af92c24e4b830963250ab1a07d6ea0
Pablo Vannini <vanninipablo@gmail.com>
2019-12-05 15:57:13 -0300

    improve welcome message, v0.1.1

--- server.js
+++ server.js
@@ -13,5 +13,19 @@ app.get('/', (req, res) => {
   console.log(Date.now(), req.query);
 });
 
+console.log('#  Asumming that you run');
+console.log('#      docker run -p PORT:8080 -it --rm --name node_logger frienddementor/general_logger');
+console.log('#  Listening on PORT all GET request and log url arguments here.');
+console.log('#  ');
+console.log('#  ');
+console.log('#  for example making the following GET request:');
+console.log('#      http://localhost:PORT/?msg1=hello&msg2=world');
+console.log('#  ');
+console.log('#  you will get the following log:');
+console.log('#      1575571784250 { msg1: \'hello\', msg2: \'world\' }');
+console.log('#  ');
+console.log('#  the timestamp of request reception following all the url arguments');
+console.log('#  ');
+console.log('  ');
+
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
